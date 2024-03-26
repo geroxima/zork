a27bc8826683a50150e91e9dbc0c5cdd9f04f8c7
Adán Alvarez <geroshima123@gmail.com>
2024-03-26 15:08:56 -0400

    feat: initialize project in netbeans

---
+++ src/Interpreter.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author geroshima
+ */
+public class Interpreter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.prinln("This will soon be Zork!");
+    }
+    
+}
